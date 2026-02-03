7b3a5f63072ba7e02f35db5ae24e18cc8303a3d6
Suprabhat Maurya <itismaurya4769@gmail.com>
2026-02-03 21:17:54 +0530

    added a method to change direction of player in order to move it

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import random
+
 import pygame
+
 
 class Ship(pygame.sprite.Sprite):
     """
     Represents the player's spaceship.
     """
+
     def __init__(self, speed, groups, window) -> None:
         """
         Initialize the ship with a specific speed.
@@ -15,28 +18,33 @@ class Ship(pygame.sprite.Sprite):
         self.direction = pygame.Vector2(1, 1)
         self.playerSpeed = speed
 
-    def update(self) -> None:
+    def update(self, x, y) -> None:
         """Update the ship's state each frame."""
-        pass
+        self.direction.update(x, y)
 
     def move(self, dt):
         """Move the ship based on its direction and delta time."""
         self.rect.centerx += self.direction.x * self.playerSpeed * dt
+        self.rect.centery += self.direction.y * self.playerSpeed * dt
+
 
 class Stars(pygame.sprite.Sprite):
     """
     Represents a background star decoration.
     """
+
     def __init__(self, x, y, groups) -> None:
         """Initialize a star at a specific position."""
         super().__init__(groups)
         self.image = pygame.image.load("./images/star.png").convert_alpha()
         self.rect = self.image.get_frect(center=(x, y))
 
+
 class SpaceShooter:
     """
     The main game class responsible for state and the game loop.
     """
+
     def __init__(self) -> None:
         """Initialize game resources, window, and entities."""
         self.window = (1920, 1080)
@@ -53,14 +61,29 @@ class SpaceShooter:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.runGame = False
+    def movePlayer(x,y):
+        self.player.update(x,y):
+        
 
     def keyboardInput(self):
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            pass
+        if keys[pygame.K_s]:
+            pass
+        if keys[pygame.K_a]:
+            pass
+        if keys[pygame.K_d]:
+            pass
 
     def createStars(self):
         """Populate the background with star sprites."""
         for i in range(self.numberOfStars):
-            Stars(random.randint(0, self.window[0]), random.randint(0, self.window[1]), self.allSprites)
+            Stars(
+                random.randint(0, self.window[0]),
+                random.randint(0, self.window[1]),
+                self.allSprites,
+            )
 
     def run(self):
         """The main game loop."""
@@ -70,11 +93,13 @@ class SpaceShooter:
             self.allSprites.draw(self.screen)
             pygame.display.flip()
 
+
 def main():
     """Entry point for the application."""
     print("welcome to space shooter")
     game = SpaceShooter()
     game.run()
+
 
 if __name__ == "__main__":
     main()
